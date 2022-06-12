19e1086ca485edb779825cf1e3e5e7e23f57c571
Jay <jaypatel70433@gmail.com>
2022-06-12 14:52:51 -0400

    Assignment4 completed

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from myapp import views
+app_name = 'myapp'
+urlpatterns = [
+ path(r'', views.index, name='index'),
+ path(r'about', views.about, name='about'),
+ path(r'<int:id>', views.details, name='detail')
+]
