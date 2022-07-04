f39d2cb8432d0f4da120444760119e9ffefbbeb4
Raj Patel <rajrpatel62499@gmail.com>
2022-07-03 21:57:42 -0400

    forms updated

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,10 +1,11 @@
-from socket import fromshare
 from django import forms
 from myapp.models import Order
 
 
 class InterestForm(forms.Form):
-    interested = forms.RadioSelect()
-    levels = forms.IntegerField(min_value=1, initial=1)
-    comments = forms.Textarea()
+    CHOICES = [(1, 'Yes'),
+               (0, 'No')]
 
+    interested = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    levels = forms.IntegerField(default=1, min_value=1)
+    comments = forms.CharField(widget=forms.Textarea, blank=True, label="Additional Comments")
